ae16652ae6e6c0dbd937e84226c9c2351cfe039e
shariski <falahudin.shariski@aignusahub.com>
2026-03-02 11:05:52 +0700

    fix logic

--- index.js
+++ index.js
@@ -5,18 +5,35 @@ class Doctor {
 		this.currentDuration = 0;
 	}
 
-	asssignPatient() {
+	assignPatient() {
 		this.currentDuration += this.avgTime;
 	}
 }
 
 function calculateWaitTime(doctors, position) {
 	let availDoctor;
-	let currentDuration = 0;
-	for (let i = 0; i < doctors.length; i++) {
-		const doctor = doctors[i];
-		if (doctor.currentDuration <= currentDuration) {
-			availDoctor = doctor;
+
+	for (let i = 1; i <= position; i++) {
+
+		availDoctor = doctors[0];
+		for (let j = 1; j < doctors.length; j++) {
+			const doctor = doctors[j];
+			if (doctor.currentDuration < availDoctor.currentDuration) {
+				availDoctor = doctor;
+			}
+			console.log({ availDoctor });
 		}
+
+		if (i == position) {
+			return availDoctor;
+		}
+
+		availDoctor.assignPatient();
 	}
 }
+
+const doctors = [new Doctor("Budi", 3), new Doctor("Aleks", 4)]
+
+const doctor = calculateWaitTime(doctors, 7)
+
+console.log({ doctor })
